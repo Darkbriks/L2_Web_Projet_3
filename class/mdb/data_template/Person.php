1c7c8954da754a9b2c65b22893c02c54ee041d0c
Darkbriks <antoine.patron.62610@gmail.com>
2024-05-29 20:29:15 +0200

    Fix Person.php

--- class/mdb/data_template/Person.php
+++ class/mdb/data_template/Person.php
@@ -17,31 +17,23 @@ class Person
     public function getHtml_Actor()
     {
         $html = "<div class='movie_actor'><p>" . $this->first_name . " " . $this->last_name . " played : " . $this->played_name . " (" . $this->birth_date ." / ";
-        if($this->death_date===null) { $html .= "still alive"; }
-        else { $html .= $this->death_date; }
-        $html .= ")</p></div>";
+        if($this->death_date===null) { $html .= "still alive"; } else { $html .= $this->death_date; } $html .= ")</p></div>";
         return $html;
     }
 
     public function getHtml_Director()
     {
         $html = "<div class='movie_director'><p>" . $this->first_name . " " . $this->last_name . " (" . $this->birth_date ." / ";
-        if($this->death_date===null) { $html .= "still alive"; }
-        else { $html .= $this->death_date; }
-        $html .= ")</p></div>";
+        if($this->death_date===null) { $html .= "still alive"; } else { $html .= $this->death_date; } $html .= ")</p></div>";
         return $html;
     }
 
     public function getHtml_list(bool $played_name = false)
     {
-        if($played_name && $this->played_name !== null) { return "<li class = 'card' style='cursor: pointer;' id='" . $this->id . "'>" . $this->first_name . " " . $this->last_name . " (" . $this->birth_date . " / " . $this->death_date . ") : " . $this->played_name . "</li>
-        <script>
-            document.getElementById('" . $this->id . "').addEventListener('click', function() { window.location.href = 'person.php?id=" . $this->id . "&fullName=" . $this->first_name . " " . $this->last_name . "'; });
-        </script>";
-        }
-        return "<li class = 'card' style='cursor: pointer;' id='" . $this->id . "'>" . $this->first_name . " " . $this->last_name . " (" . $this->birth_date . " / " . $this->death_date . ")</li>
-        <script>
-            document.getElementById('" . $this->id . "').addEventListener('click', function() { window.location.href = 'person.php?id=" . $this->id . "&fullName=" . $this->first_name . " " . $this->last_name . "'; });
-        </script>";
+        $html = "<li class = 'card' style='cursor: pointer;' id='" . $this->id . "'>" . $this->first_name . " " . $this->last_name . " (" . $this->birth_date . " / ";
+        if($this->death_date===null) { $html .= "still alive)"; } else { $html .= $this->death_date . ")"; }
+        if($played_name && $this->played_name !== null) { $html .= " : " . $this->played_name; } $html .= "</li>
+        <script>document.getElementById('" . $this->id . "').addEventListener('click', function() { window.location.href = 'person.php?id=" . $this->id . "&fullName=" . $this->first_name . " " . $this->last_name . "'; });</script>";
+        return $html;
     }
 }
