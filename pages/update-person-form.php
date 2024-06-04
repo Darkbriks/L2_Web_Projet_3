a6e5346d6f18d56dc5586e5b942f181daf917c65
Zyre28 <babies11@hotmail.fr>
2024-06-04 15:30:06 +0200

    fix button (work 3/4), person

--- pages/update-person-form.php
+++ pages/update-person-form.php
@@ -16,8 +16,8 @@ $persons = $personDB->getPersons(); // Récupère toutes les personnes existante
                 </div>
                 <div class="modal-body">
                     <form method='POST' enctype='multipart/form-data' class="add-person-form">
-                        <label for="person_id">Choisir une personne à mettre à jour :</label>
-                        <select name="person_id" id="person_id">
+                        <label for="update_person_id">Choisir une personne à mettre à jour :</label>
+                        <select name="update_update_person_id" id="update_person_id">
                             <?php foreach ($persons as $person) { ?>
                                 <option value="<?php echo $person->getId(); ?>"><?php echo $person->getFirstName() . " " . $person->getLastName(); ?></option>
                             <?php } ?>
@@ -62,15 +62,15 @@ $persons = $personDB->getPersons(); // Récupère toutes les personnes existante
     </div>
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['person_id'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update_person_id'])) {
 
-    $person_id = $_POST['person_id'];
+    $update_person_id = $_POST['update_person_id'];
 
     if (isset($_POST['delete_person'])) {
     // Vérifier si l'ID du personnage à supprimer est présent dans la requête
 
         // Appeler la fonction pour supprimer la personne avec l'ID spécifié
-        $success = $personDB->deletePersonAndRelationsById( $person_id);
+        $success = $personDB->deletePersonAndRelationsById( $update_person_id);
 
         if (!empty($success)) {
             echo "Le personnage a été supprimé avec succès.";
@@ -91,28 +91,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['person_id'])) {
         isset($_FILES['new_image_path'])
     ) {
         // Récupérer les valeurs des champs
-        $new_first_name = $_POST['new_first_name'];
-        $new_last_name = $_POST['new_last_name'];
-        $new_birth_date = $_POST['new_birth_date'];
-        $new_death_date = $_POST['new_death_date'];
-        $new_image_name = $_FILES['new_image_path']['name'];
-        $new_image_tmp_path = $_FILES['new_image_path']['tmp_name'];
+        $person_id = htmlspecialchars(trim($_POST['person-id']));
+        $new_first_name = htmlspecialchars(trim($_POST['person-first-name']));
+        $new_last_name = htmlspecialchars(trim($_POST['person-last-name']));
+        $new_birth_date = htmlspecialchars(trim($_POST['person-birth-date']));
+        $new_death_date = !empty($_POST['person-death-date']) ? htmlspecialchars(trim($_POST['person-death-date'])) : null;
+        $new_image_name = '';
 
-        // Définir le chemin où l'image sera stockée sur le serveur
-        $target_dir = "uploads/";
-        $new_image_path = $target_dir . basename($new_image_name);
-
-        // Traiter l'image si elle a été téléchargée
-        if ($_FILES['new_image_path']['size'] > 0) {
-            // Déplacer l'image téléchargée vers le dossier d'uploads
-            move_uploaded_file($new_image_tmp_path, $new_image_path);
+        if (!empty($_FILES['person-image']['name'])) {
+            $img_file = $_FILES['person-image'];
+            $tmp_name = $img_file['tmp_name'];
+            $img_name = $img_file['name'];
+            $img_name = urldecode(htmlspecialchars($img_name));
+            $dir = $GLOBALS['PHP_DIR'] . 'uploads/peoples/';
+            if (!is_dir($dir)) {
+                mkdir($dir, 0777, true);
+            }
+            if (move_uploaded_file($tmp_name, $dir . $img_name)) {
+                $new_image_name = $img_name;
+            } else {
+                $update_error = $GLOBALS['person-form-exception-upload'];
+            }
         }
 
         // Valider le formulaire
-        if (checkValues($person_id, $new_first_name, $new_last_name, $new_birth_date, $new_death_date, $new_image_name)) {
+        if (checkValues($update_person_id, $new_first_name, $new_last_name, $new_birth_date, $new_death_date, $new_image_name)) {
 
             // Si le formulaire est valide, vous pouvez maintenant procéder à la mise à jour de la personne dans la base de données
-            $success = $personDB->alterPerson($person_id, $new_first_name, $new_last_name, $new_birth_date, $new_death_date, $new_image_path);
+            $success = $personDB->alterPerson($update_person_id, $new_first_name, $new_last_name, $new_birth_date, $new_death_date, $new_image_name);
             if (!empty($success)) {
                 echo "Les informations de la personne ont été mises à jour avec succès.";
             } else {
@@ -128,10 +134,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['person_id'])) {
     }
 }
 
-function checkValues($person_id, $new_first_name, $new_last_name, $new_birth_date, $new_death_date, $new_image_name)
+function checkValues($update_person_id, $new_first_name, $new_last_name, $new_birth_date, $new_death_date, $new_image_name)
 {
     // Vérification si tous les champs requis sont remplis
-    if (empty($person_id) || empty($new_first_name) || empty($new_last_name) || empty($new_birth_date) || empty($new_image_name)) {
+    if (empty($update_person_id) || empty($new_first_name) || empty($new_last_name) || empty($new_birth_date) || empty($new_image_name)) {
         echo "Tous les champs requis doivent être remplis.";
         return false;
     }
