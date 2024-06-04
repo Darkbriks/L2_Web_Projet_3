817af620466bc9492d69c0afb01890641b740cbf
Touhaf <Touhafaya@gmail.com>
2024-06-04 15:41:55 +0200

    link for eye icone on template modification of personDB for checking person existence

--- class/Template.php
+++ class/Template.php
@@ -13,6 +13,7 @@ class Template
             <title><?php echo $GLOBALS['template-title'] ?></title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" rel="stylesheet">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
             <link rel="stylesheet" href="../css/my_movies.css">
         </head>
