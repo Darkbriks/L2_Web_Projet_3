e8bad8a49e8c9176fb75172567f248cee9501d3c
Touhaf <Touhafaya@gmail.com>
2024-06-06 17:18:25 +0200

    footer done with sources and github link plus css modified

--- pages/footer.php
+++ pages/footer.php
@@ -1,5 +1,36 @@
-<footer class ="footer">
-    <div class="container-fluid">
-    <p><?php echo $GLOBALS['footer-text'] ?></p>
+<footer class="footer text-white py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p class="mb-2">
+                    <i class="fab fa-github"></i>
+                    <a href="https://github.com/Darkbriks/L2_Web_Projet_3" target="_blank" class="text-white">Github</a>
+                </p>
+                <p class="mb-2">Team: Coding Gladiators</p>
+            </div>
+            <div class="col-md-6">
+                <?php echo $GLOBALS['footer-text'] ?>
+                <p class="mb-2">
+                    <img src="../uploads/mr_bourguin.png" alt="Professor Gregory Bourguin" class="rounded-circle" style="width: 30px; height: 30px;">
+                    <a href="http://web.gregory-bourguin.fr" target="_blank" class="text-white">Gregory Bourguin</a>
+                </p>
+                <p class="mb-2">
+                    <i class="fab fa-bootstrap"></i>
+                    <a href="https://getbootstrap.com/" target="_blank" class="text-white">Bootstrap</a>
+                </p>
+                <p class="mb-2">
+                    <i class="fab fa-font-awesome"></i>
+                    <a href="https://fontawesome.com/" target="_blank" class="text-white">Font Awesome</a>
+                </p>
+                <p class="mb-2">
+                    <i class="fab fa-youtube"></i>
+                    <a href="https://www.youtube.com/" target="_blank" class="text-white">YouTube</a>
+                </p>
+               <p class = "mb-2">
+                   <i class="fab fa-wikipedia-w"></i>
+                   <a href="https://www.wikipedia.org/" target="_blank" class="text-white">Wikipedia</a>
+                </p>
+            </div>
+        </div>
     </div>
 </footer>
