9e2639e42f5e5d862ecaef1f017e11e54a755f54
Touhaf <Touhafaya@gmail.com>
2024-06-07 15:14:17 +0200

    favicon added

--- class/Template.php
+++ class/Template.php
@@ -16,6 +16,12 @@ class Template
             <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
             <link rel="stylesheet" href="../css/my_movies.css">
+            <link rel="apple-touch-icon" sizes="180x180" href="../uploads/favicon/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="../uploads/favicon/favicon-32x32.png">
+            <link rel="manifest" href="../uploads/favicon/site.webmanifest">
+            <link rel="mask-icon" href="../uploads/favicon/safari-pinned-tab.svg" color="#5bbad5">
+            <meta name="msapplication-TileColor" content="#da532c">
+            <meta name="theme-color" content="#ffffff">
         </head>
         <body>
             <?php include "header.php" ?>
